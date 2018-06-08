ce2e436c735c1afe846303a5f8e637a9923f263e
Eddy Segura <eseguran@outlook.com>
2018-06-08 16:12:14 -0600

    inicial

---
+++ Pods/Target Support Files/Pods-asodxc/Pods-asodxc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_asodxc : NSObject
+@end
+@implementation PodsDummy_Pods_asodxc
+@end
--- asodxc/ViewController.swift
+++ asodxc/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
+ 
+    
+  
+    
+    
+    @IBOutlet weak var pass: UITextField!
+    
+    @IBOutlet weak var user: UITextField!
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var NID: UITextField!
+    
+    @IBOutlet weak var regBtn: UIButton!
+    
+    @IBAction func regBtn_Click(_ sender: Any) {
+        
+    
+            if self.user.text != "" {
+                let usuario: Usuario = Usuario()
+                usuario.UN = self.user.text!
+                usuario.password = self.pass.text!
+                usuario.name = self.name.text!
+                usuario.NID = self.NID.text!
+                try! realm.write {
+                    realm.add(usuario)
+                }
+            
+        }
+        
+        
+    }
+    
+    var realm: Realm!
+    
+    var objectsArray: Results<Usuario> {
+        get {
+            return realm.objects(Usuario.self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        realm = try! Realm()
     }
 
+
+
+    
+    
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+ 
 
+}
+
+class Usuario: Object {
+    @objc dynamic var Id: String = UUID().uuidString
+    @objc dynamic var name: String = ""
+    @objc dynamic var UN: String = ""
+    @objc dynamic var password: String = ""
+    @objc dynamic var NID: String = ""
+    override static func primaryKey() -> String? {
+        return "Id"
+    }
 }
 
